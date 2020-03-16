a6387a91ac3748b2c950f33ff25607c8ea810582
egert <egert@localhost.localdomain>
2020-03-16 13:20:30 +0200

    menu done, added some assets

--- typeform/src/App.js
+++ typeform/src/App.js
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import cart from "./assets/cart.png";
+import login from "./assets/login.png";
+import searchIcon from "./assets/searchIcon.png";
+import sydneyOperaHouse from "./assets/sydney-opera-house.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="soh-page">
+        {/* header */}
+        <div className="header-top">
+            {/* logo*/}
+            <h1 className="header-logo">
+                <a href="/">
+                    <img className="img-svg" src={sydneyOperaHouse} alt="Sydney Opera House" aria-hidden="true"></img>
+                </a>
+            </h1>
+            {/* search */}
+            <div className="header-search">
+                <div className="search-container">
+                    <input id="search" placeholder="Search for ..."></input>
+                    <button className="search-button">
+                        <img src={searchIcon} style={{paddingLeft: "20px"}}></img>
+                    </button>
+                </div>
+            </div>
+
+            {/* menu */}
+            <nav className="header-menu">
+              <ul>
+                  <li>
+                      <a href="/events.html">Events</a>
+                  </li>
+
+                  <li>
+                      <a href="/visit-us.html">Visit Us</a>
+                  </li>
+
+                  <li>
+                      <a href="/our-story.html">Our Story</a>
+                  </li>
+
+                  <li>
+                      <a href="/give.html">Give</a>
+                  </li>
+
+                  <li>
+                      <a href="/backstage.html">Backstage</a>
+                  </li>
+
+                  <li>
+                      <a href="/schools.html">Schools </a>
+                  </li>
+              </ul>
+            </nav>
+            {/* login and cart */}
+            <div className="header-icon header-login">
+                <img src={login} ></img>
+                <a href="" className="header-login login">Login</a>
+            </div>
+
+            <div className="header-icon header-shopping">
+                <img src={cart} style={{mariginTop: "-3px"}} ></img>
+                <a href="" className="header-login shop">Cart</a>
+            </div>
+        </div>
+        {/* video */}
+        <div className="hero">
+            <h1>Welcome to our House</h1>
+            <p>A place to take in a spectacular horizon, then broaden your own.</p>
+        </div>
     </div>
   );
 }
