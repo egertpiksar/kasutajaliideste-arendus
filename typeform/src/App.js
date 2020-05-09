98996698ba223d01c9a458e27430a2997e5d29cb
Egert <piksar@tlu.ee>
2020-05-09 12:27:35 +0300

    menu and rightsidebar

--- typeform/src/App.js
+++ typeform/src/App.js
@@ -18,10 +18,65 @@ import opera from "./assets/OperaAustralia.png";
 
 import instagram from "./assets/instagram.png";
 
+function openNav(){
+  const sidebar = document.querySelector(".right-sidebar");
+  sidebar.classList.toggle("is-active");
+
+  const overlay = document.querySelector(".right-sidebar-overlay");
+  overlay.classList.toggle("is-active");
+}
+
+function closeNav(){
+  if (openNav()){
+    this.overlay.removeClass("is-active");
+    this.sidebar.removeClass("is-active");
+  }
+}
+
 
 function App() {
   return (
     <div className="soh-page">
+        <div className="right-sidebar-overlay"></div>
+        <div className="right-sidebar">
+          <div className="close-sidebar">
+              <div className="close-sidebar-btn" onClick={ () => closeNav()}>
+                X
+              </div>
+            </div>
+
+          <nav>            
+            <ul>
+              <li>
+                <a href="/digital.html">Digital</a>
+              </li>
+
+              <li>
+                <a href="/give.html">Give</a>
+              </li>
+
+              <li>
+                <a href="/events.html">Events</a>
+              </li>
+
+              <li>
+                <a href="/events.html">Visit Us</a>
+              </li>
+
+              <li>
+                <a href="/our-story.html">Our Story</a>
+              </li>
+              
+              <li>
+                 <a href="/schools.html">Schools </a>
+              </li>
+
+              <li>
+                  <a href="/account.html">Account</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         {/* header */}
         <div className="header-top">
             {/* logo*/}
@@ -68,6 +123,13 @@ function App() {
                   </li>
               </ul>
             </nav>
+
+            <div className="hamburger" onClick={ () => openNav()}>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+
             {/* login and cart */}
             <div className="header-icon header-login">
                 <img src={login} alt="img"></img>
@@ -184,14 +246,14 @@ function App() {
                 <ul>
                   <li>
                     {/* copied */}
-                      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">
+                      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" className="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">
                       <path id="_Compound_Path_" data-name="Compound Path" d="M36.86 49.82h-9.8V32.24h9.8V20.93c0-8.11 3.85-20.81 20.8-20.81h15.28v17H61.86c-1.81 0-4.37.9-4.37 4.77v10.3h15.72L71.4 49.82H57.49v49.61H36.86z"></path>
                       </svg>
                   </li>
 
                   <li>
                     {/* copied */}
-                      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">   
+                      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" className="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">   
                         <path id="_Compound_Path_" data-name="Compound Path" d="M5.13 81.61a41.13 41.13 0 0 0 25.46-8.77A20.53 20.53 0 0 1 11.44 58.6a20.35 20.35 0 0 0 9.25-.35A20.53 20.53 0 0 1 4.26 38.13v-.25a20.39 20.39 0 0 0 9.29 2.53A20.53 20.53 0 0 1 7.23 13a58.14 58.14 0 0 0 42.22 21.49A20.51 20.51 0 0 1 84.33 15.8a41 41 0 0 0 13-5 20.58 20.58 0 0 1-9 11.38A40.84 40.84 0 0 0 100.1 19a41.58 41.58 0 0 1-10.23 10.59v2.65c0 27.1-20.63 58.34-58.34 58.34A58.06 58.06 0 0 1 .1 81.37a43.26 43.26 0 0 0 5.03.24z">
                         </path>
                       </svg>
@@ -199,7 +261,7 @@ function App() {
 
                   <li>
                     {/* copied */}
-                      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">
+                      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" className="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">
                           <path id="_Compound_Path_" data-name="Compound Path" d="M79 72.44h-4.51v-2.52a3.79 3.79 0 0 1 .51-2.28 2 2 0 0 1 1.73-.7 2 2 0 0 1 1.68.7 3.79 3.79 0 0 1 .59 2.28zM59.61 84.8a1.66 1.66 0 0 0 1.39-.54 2.8 2.8 0 0 0 .4-1.75V69.43a3 3 0 0 0-.53-1.95 2 2 0 0 0-1.57-.66 2.53 2.53 0 0 0-1.08.24 3.79 3.79 0 0 0-1 .77v15.8a4.43 4.43 0 0 0 1.26.9 2.92 2.92 0 0 0 1.13.27zm-33-45.94H33V23.2l7.45-22.12h-6.51L30 16.2h-.4L25.4 1.08h-6.46l7.59 22.77v15zM0 80.6V66a18.26 18.26 0 0 1 18.25-18.28h63.49A18.28 18.28 0 0 1 100 66v14.6a18.28 18.28 0 0 1-18.24 18.32H18.25A18.26 18.26 0 0 1 0 80.64zm69.29.66A8.07 8.07 0 0 0 71.25 87a7 7 0 0 0 5.31 2.12 7.59 7.59 0 0 0 5.68-1.95 8.11 8.11 0 0 0 1.91-5.88V80H79v1.15a5.49 5.49 0 0 1-.51 2.88 2.09 2.09 0 0 1-1.76.66A1.9 1.9 0 0 1 75 84a5.55 5.55 0 0 1-.49-2.78v-4.83h9.66v-6.46a7.59 7.59 0 0 0-1.87-5.51 7.11 7.11 0 0 0-5.34-1.92 7.59 7.59 0 0 0-5.54 2.07 7.12 7.12 0 0 0-2.15 5.36v11.38zM52.1 88.39h5.06V86.5a6.48 6.48 0 0 0 2.19 1.69 6 6 0 0 0 2.64.54 4.2 4.2 0 0 0 3.44-1.43 6.32 6.32 0 0 0 1.19-4.12v-14a7.59 7.59 0 0 0-1.26-4.73 4.36 4.36 0 0 0-3.67-1.61 5.06 5.06 0 0 0-2.34.59 8 8 0 0 0-2.16 1.76V54.13h-5.06v34.3zm-18.28-4.11a5.45 5.45 0 0 0 .89 3.39 3.15 3.15 0 0 0 2.64 1.14 6.11 6.11 0 0 0 2.94-.81 10.75 10.75 0 0 0 2.92-2.34v2.78h5.06V63.15h-5.06v19.22a7 7 0 0 1-1.57 1.37 2.86 2.86 0 0 1-1.4.53 1.26 1.26 0 0 1-1-.4 2.16 2.16 0 0 1-.32-1.26V63.12h-5.1zM14.09 59.1H20v29.33h5.7V59.1h5.9v-5H14.09zm29.09-21.63a7.4 7.4 0 0 1-2.24-5.65V17.36a6.32 6.32 0 0 1 2.29-5.16 9.36 9.36 0 0 1 6.19-1.92 8.39 8.39 0 0 1 5.78 2 6.7 6.7 0 0 1 2.25 5.21v14.45a7.27 7.27 0 0 1-2.21 5.65 8.7 8.7 0 0 1-6.11 2 8.39 8.39 0 0 1-6-2.15zm3.62-5.12a2.43 2.43 0 0 0 .61 1.77 2.24 2.24 0 0 0 1.73.63 2.53 2.53 0 0 0 1.86-.64 2.3 2.3 0 0 0 .7-1.77V17.16a1.83 1.83 0 0 0-.7-1.48 2.85 2.85 0 0 0-1.83-.56 2.53 2.53 0 0 0-1.69.56 1.9 1.9 0 0 0-.65 1.48v15.18zm19.28 6.94a7.21 7.21 0 0 0 3.3-.9 12.07 12.07 0 0 0 3.28-2.61v3.07h5.7V11h-5.73v21.17a7.89 7.89 0 0 1-1.75 1.49 3.31 3.31 0 0 1-1.59.58 1.39 1.39 0 0 1-1.18-.44 2.3 2.3 0 0 1-.4-1.42V11H62v23.31A5.83 5.83 0 0 0 63 38a3.58 3.58 0 0 0 3 1.26z">
                           </path>
                       </svg>
@@ -211,7 +273,7 @@ function App() {
 
                   <li>
                     {/* copied */}
-                    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">
+                    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" className="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">
                         <defs>
                            <style></style>
                         </defs>
@@ -231,7 +293,7 @@ function App() {
 
                   <li>
                       {/* copied */}
-                      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">
+                      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" className="img-svg replaced-svg" style={{width: "30px", height: "30px"}} aria-hidden="true" focusable="false">
                           <path d="M73.44 50.12c-1.37-.42-2.31-.71-1.6-2.54 1.56-4 1.72-7.42 0-9.87-3.16-4.6-11.82-4.35-21.74-.12 0 0-3.12 1.39-2.32-1.13 1.53-5 1.29-9.17-1.08-11.59-5.38-5.48-19.69.21-32 12.7C5.59 46.92.26 56.84.26 65.41c0 16.4 20.66 27.37 40.87 27.37 26.5 0 44.13-16.67 44.13-29.11 0-7.52-6.26-11.79-11.82-13.55zM41.18 85.43C25.05 87 11.13 79.7 10.08 69s11.19-20.53 27.31-22.13 30.06 5.73 31.1 16.37-11.17 20.58-27.31 22.19zm58.58-48.66a30 30 0 0 0-30-30 3.5 3.5 0 0 0 0 7 23 23 0 0 1 23 23 3.5 3.5 0 0 0 7 0zm-13.91-.51a20 20 0 0 0-15.57-15.57 3.5 3.5 0 0 0-1.41 6.86A13 13 0 0 1 79 37.66a3.5 3.5 0 0 0 6.86-1.41zm-52.94 21C26.41 58.6 22 64 23.12 69.46s7.24 8.75 13.74 7.44 10.88-6.75 9.79-12.17-7.24-8.73-13.74-7.44z" id="_Group_" data-name="<Group>"></path>
                       </svg>
                   </li>
